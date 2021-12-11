76a8d45fca49d7db8af49e9589a954583a95d3c5
Ffloriel <florielfedry@gmail.com>
2021-12-11 12:05:48 +0000

    build: add dev/production builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,40 @@
+import replace from "@rollup/plugin-replace";
 import typescript from "rollup-plugin-ts";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "src/index.ts",
-  plugins: [typescript(), terser()],
-  output: [
-    {
-      file: "./lib/postcss-debug-borders.esm.js",
-      format: "esm",
-      exports: "auto"
-    },
-    {
-      file: "./lib/postcss-debug-borders.js",
-      format: "cjs",
-      exports: "auto"
-    },
-  ],
-};
+function getConfig(isDevelopment) {
+  const fileName = isDevelopment
+    ? "postcss-debug-borders.development"
+    : "postcss-debug-borders";
+
+  return {
+    input: "src/index.ts",
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          __DEVELOPMENT__: isDevelopment
+        }
+      }),
+      typescript(),
+      terser(),
+    ],
+    output: [
+      {
+        file: `./lib/${fileName}.esm.js`,
+        format: "esm",
+        exports: "auto"
+      },
+      {
+        file: `./lib/${fileName}.js`,
+        format: "cjs",
+        exports: "auto"
+      },
+    ],
+  };
+}
+
+export default [
+  getConfig(false),
+  getConfig(true)
+];
